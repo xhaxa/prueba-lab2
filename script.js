6f80507ea80c1669dd26b026461d3b904bf38b1f
Jorge <jorlorsan@gmail.com>
2021-02-10 22:27:51 +0000

    Con mensajito

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ var myElement = document.getElementById(“button - yellow”);
 myElement.addEventListener(“click”, function() {
             var myEmail = window.prompt(“GIVE ME YOUR EMAIL please”);
         };
+
+
+        var btn = document.getElementById("mybtn");
+btn.value = 'my value'; // will just add a hidden value
+btn.innerHTML = 'Thank you for your email';
