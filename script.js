2c4c2741574384bf87a03167c3cf8e28ee5b2d2e
Sara <you@example.comxhaxadesign@gmail.com>
2021-02-10 22:03:23 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var myElement = document.getElementById(“button - yellow”);
+myElement.addEventListener(“click”, function() {
+            var myEmail = window.prompt(“GIVE ME YOUR EMAIL please”);
+        };
